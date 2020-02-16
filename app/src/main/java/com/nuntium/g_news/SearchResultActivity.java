a896774e56b5098291dafbc4c5103a37d4e237a8
Robert <pvu9@my.bcit.ca>
2020-02-16 12:41:32 -0800

    Initial commit.

---
+++ app/src/main/java/com/nuntium/g_news/SearchResultActivity.java
@@ -0,0 +1,14 @@
+package com.nuntium.g_news;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class SearchResultActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/nuntium/g_news/model/Article.java
@@ -0,0 +1,11 @@
+package com.nuntium.g_news.model;
+
+import java.util.Date;
+
+public class Article {
+    private String title;
+    private String source;
+    private String author;
+    private Date publishedAt;
+    private String urlToImage;
+}
